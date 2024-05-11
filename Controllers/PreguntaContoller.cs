46ab2dd6c4a7e673a3d23df07d8d56e5685211dc
Diego Guinguer <dguinguer@gmail.com>
2024-05-10 22:46:44 -0500

    Feat: Creacionde la Pregunta service y controller, ademas se agrego el método get preguntas

---
+++ Controllers/PreguntaContoller.cs
@@ -0,0 +1,31 @@
+﻿using apiCuestionario.Models.Entities;
+using apiCuestionario.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiCuestionario.Controllers
+{
+    [ApiController]
+    [Route("api/pregunta")]
+    public class PreguntaController : ControllerBase
+    {
+        private readonly IPreguntaService _preguntaService;
+
+        public PreguntaController(IPreguntaService preguntaService)
+        {
+            _preguntaService = preguntaService;
+        }
+
+        [HttpGet]
+        public IActionResult ListarPreguntas()
+        {
+            if(_preguntaService.ListarPreguntas().Any())
+            {
+                return Ok(_preguntaService.ListarPreguntas());
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ Services/PreguntaService.cs
@@ -0,0 +1,25 @@
+﻿using apiCuestionario.Models.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace apiCuestionario.Services
+{
+    public interface IPreguntaService
+    {
+        IEnumerable<Pregunta> ListarPreguntas();
+    }
+
+    public class PreguntaService: IPreguntaService
+    {
+        CuestionarioContext db;
+
+        public PreguntaService(CuestionarioContext db)
+        {
+            this.db = db;
+        }
+
+        public IEnumerable<Pregunta> ListarPreguntas()
+        {
+            return db.Preguntas;
+        }
+    }
+}
